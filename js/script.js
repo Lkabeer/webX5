da790346f3bb020046f2b1ecb652af138f635df3
Lkabeer <ahmed@Lkabeer.com>
2018-09-22 10:58:45 +0200

    version X0.3

--- js/script.js
+++ js/script.js
@@ -22,17 +22,3 @@ window.addEventListener('load', function() {
     slideShowX.style.display = 'block';
     console.log('All assets are loaded')
 });
-
-// Google Speed insights Optimize CSS Delivery X-Team
-var loadDeferredStyles = function() {
-    var addStylesNode = document.getElementById("deferred-styles");
-    var replacement = document.createElement("div");
-    replacement.innerHTML = addStylesNode.textContent;
-    document.body.appendChild(replacement)
-    addStylesNode.parentElement.removeChild(addStylesNode);
-  };
-  var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-      window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-  if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
-  else window.addEventListener('load', loadDeferredStyles);
-// End Google Speed insights Optimize CSS Delivery X-Team

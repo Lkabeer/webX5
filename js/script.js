4549c6ea62476c8ad67fe2a8501f0f261a0a2b3d
Lkabeer <ahmed@Lkabeer.com>
2018-09-22 10:51:12 +0200

    version X0.2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
+var spinnerX = document.querySelector('#spinnerX');
+var slideShowX = document.querySelector('#slideShowX');
+
 var next = document.getElementById('next');
 var pre = document.getElementById('pre');
-var slideShowX = document.getElementById('slideShowX');
 
 function nextImage() {
-    slideShowX.style.backgroundImage = "url('../assets/bmw101.jpg')";
+    slideShowX.style.backgroundImage = "url('./assets/bmw101.jpg')";
 }
 
 function preImage() {
-    slideShowX.style.backgroundImage = "url('../assets/bmw102.jpg')";
+    slideShowX.style.backgroundImage = "url('./assets/bmw102.jpg')";
 }
+
+// setTimeout(() => {
+//     spinnerX.style.display = 'none';
+//     slideShowX.style.display = 'block';
+// }, 2000);
+
+window.addEventListener('load', function() {
+    spinnerX.style.display = 'none';
+    slideShowX.style.display = 'block';
+    console.log('All assets are loaded')
+});
